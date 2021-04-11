6bd8ada3de6a157dfc1f888200ef862c3fb23673
Brandon Bawe <bawe.damue@gmail.com>
2021-04-11 04:09:04 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connected to database"));
+
+app.use(express.json());
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+
+app.listen(3000, () => console.log("server started"));
